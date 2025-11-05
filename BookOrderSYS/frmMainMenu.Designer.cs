e85aae63ffe69c609044632f3b0d4d081765184b
GenesisPrototype <shinemilsen@gmail.com>
2025-11-05 10:33:30 +0000

    Add project files.

---
+++ BookOrderSYS/frmMainMenu.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace BookOrderSYS
+{
+    partial class frmMainMenu
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.mnuMainMenu = new System.Windows.Forms.MenuStrip();
+            this.mnuCustomers = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuBooks = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOrders = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuExit = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMainMenu.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // mnuMainMenu
+            // 
+            this.mnuMainMenu.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuCustomers,
+            this.mnuBooks,
+            this.mnuOrders,
+            this.mnuExit});
+            this.mnuMainMenu.Location = new System.Drawing.Point(0, 0);
+            this.mnuMainMenu.Name = "mnuMainMenu";
+            this.mnuMainMenu.Size = new System.Drawing.Size(800, 24);
+            this.mnuMainMenu.TabIndex = 0;
+            this.mnuMainMenu.Text = "menuStrip1";
+            // 
+            // mnuCustomers
+            // 
+            this.mnuCustomers.Name = "mnuCustomers";
+            this.mnuCustomers.Size = new System.Drawing.Size(76, 20);
+            this.mnuCustomers.Text = "Customers";
+            // 
+            // mnuBooks
+            // 
+            this.mnuBooks.Name = "mnuBooks";
+            this.mnuBooks.Size = new System.Drawing.Size(51, 20);
+            this.mnuBooks.Text = "Books";
+            // 
+            // mnuOrders
+            // 
+            this.mnuOrders.Name = "mnuOrders";
+            this.mnuOrders.Size = new System.Drawing.Size(54, 20);
+            this.mnuOrders.Text = "Orders";
+            // 
+            // mnuExit
+            // 
+            this.mnuExit.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.mnuExit.Name = "mnuExit";
+            this.mnuExit.Size = new System.Drawing.Size(37, 20);
+            this.mnuExit.Text = "Exit";
+            // 
+            // frmMainMenu
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.mnuMainMenu);
+            this.MainMenuStrip = this.mnuMainMenu;
+            this.Name = "frmMainMenu";
+            this.Text = "Booktopia - [Main Menu]";
+            this.Load += new System.EventHandler(this.frmMainMenu_Load);
+            this.mnuMainMenu.ResumeLayout(false);
+            this.mnuMainMenu.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip mnuMainMenu;
+        private System.Windows.Forms.ToolStripMenuItem mnuCustomers;
+        private System.Windows.Forms.ToolStripMenuItem mnuBooks;
+        private System.Windows.Forms.ToolStripMenuItem mnuOrders;
+        private System.Windows.Forms.ToolStripMenuItem mnuExit;
+    }
+}
+
